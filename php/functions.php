24e73107381d542149fa6ea5a31d9ec622c30580
Darren Hankins <hankins.darren@gmail.com>
2018-01-21 08:42:22 -0700

    updated functions.php with reduced cpt and taxes

--- php/functions.php
+++ php/functions.php
@@ -22,40 +22,6 @@ add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
 function cptui_register_my_cpts() {
 
 	/**
-	 * Post Type: Services.
-	 */
-
-	$labels = array(
-		"name" => __( "Services", "uncode-child" ),
-		"singular_name" => __( "Service", "uncode-child" ),
-	);
-
-	$args = array(
-		"label" => __( "Services", "uncode-child" ),
-		"labels" => $labels,
-		"description" => "",
-		"public" => true,
-		"publicly_queryable" => true,
-		"show_ui" => true,
-		"show_in_rest" => false,
-		"rest_base" => "",
-		"has_archive" => false,
-		"show_in_menu" => true,
-		"show_in_menu_string" => "10",
-		"exclude_from_search" => false,
-		"capability_type" => "post",
-		"map_meta_cap" => true,
-		"hierarchical" => false,
-		"rewrite" => array( "slug" => "services", "with_front" => true ),
-		"query_var" => true,
-		"menu_icon" => "dashicons-store",
-		"supports" => array( "title", "editor", "thumbnail", "excerpt" ),
-		"taxonomies" => array( "category", "post_tag" ),
-	);
-
-	register_post_type( "services", $args );
-
-	/**
 	 * Post Type: Team Members.
 	 */
 
@@ -89,72 +55,6 @@ function cptui_register_my_cpts() {
 	register_post_type( "team", $args );
 
 	/**
-	 * Post Type: Events.
-	 */
-
-	$labels = array(
-		"name" => __( "Events", "uncode-child" ),
-		"singular_name" => __( "Event", "uncode-child" ),
-	);
-
-	$args = array(
-		"label" => __( "Events", "uncode-child" ),
-		"labels" => $labels,
-		"description" => "",
-		"public" => true,
-		"publicly_queryable" => true,
-		"show_ui" => true,
-		"show_in_rest" => false,
-		"rest_base" => "",
-		"has_archive" => false,
-		"show_in_menu" => true,
-		"exclude_from_search" => false,
-		"capability_type" => "post",
-		"map_meta_cap" => true,
-		"hierarchical" => false,
-		"rewrite" => array( "slug" => "events", "with_front" => true ),
-		"query_var" => true,
-		"menu_icon" => "dashicons-palmtree",
-		"supports" => array( "title", "editor", "thumbnail", "excerpt", "custom-fields" ),
-		"taxonomies" => array( "category", "post_tag", "event_category" ),
-	);
-
-	register_post_type( "events", $args );
-
-	/**
-	 * Post Type: Press Writeups.
-	 */
-
-	$labels = array(
-		"name" => __( "Press Writeups", "uncode-child" ),
-		"singular_name" => __( "Press Writeup", "uncode-child" ),
-	);
-
-	$args = array(
-		"label" => __( "Press Writeups", "uncode-child" ),
-		"labels" => $labels,
-		"description" => "",
-		"public" => true,
-		"publicly_queryable" => true,
-		"show_ui" => true,
-		"show_in_rest" => false,
-		"rest_base" => "",
-		"has_archive" => false,
-		"show_in_menu" => true,
-		"exclude_from_search" => false,
-		"capability_type" => "post",
-		"map_meta_cap" => true,
-		"hierarchical" => false,
-		"rewrite" => array( "slug" => "press", "with_front" => true ),
-		"query_var" => true,
-		"menu_icon" => "dashicons-megaphone",
-		"supports" => array( "title", "editor", "thumbnail", "excerpt" ),
-		"taxonomies" => array( "category", "post_tag", "press_category" ),
-	);
-
-	register_post_type( "press", $args );
-
-	/**
 	 * Post Type: Careers.
 	 */
 
@@ -186,39 +86,6 @@ function cptui_register_my_cpts() {
 	);
 
 	register_post_type( "careers", $args );
-
-	/**
-	 * Post Type: Locations.
-	 */
-
-	$labels = array(
-		"name" => __( "Locations", "uncode-child" ),
-		"singular_name" => __( "Location", "uncode-child" ),
-	);
-
-	$args = array(
-		"label" => __( "Locations", "uncode-child" ),
-		"labels" => $labels,
-		"description" => "",
-		"public" => true,
-		"publicly_queryable" => true,
-		"show_ui" => true,
-		"show_in_rest" => false,
-		"rest_base" => "",
-		"has_archive" => false,
-		"show_in_menu" => true,
-		"exclude_from_search" => false,
-		"capability_type" => "post",
-		"map_meta_cap" => true,
-		"hierarchical" => false,
-		"rewrite" => array( "slug" => "locations", "with_front" => true ),
-		"query_var" => true,
-		"menu_icon" => "dashicons-location",
-		"supports" => array( "title", "editor", "thumbnail", "excerpt" ),
-		"taxonomies" => array( "category", "post_tag", "location_category" ),
-	);
-
-	register_post_type( "locations", $args );
 
 	/**
 	 * Post Type: NPV Improvements.
@@ -261,9 +128,9 @@ add_action( 'init', 'cptui_register_my_cpts' );
 
 function cptui_register_my_taxes() {
 
-  /********************************
-  	* TEAM MEMBER CATEGORIES
-  ********************************/
+	/**
+	 * Taxonomy: Team Member Categories.
+	 */
 
 	$labels = array(
 		"name" => __( "Team Member Categories", "uncode-child" ),
@@ -288,9 +155,9 @@ function cptui_register_my_taxes() {
 	);
 	register_taxonomy( "team_member_category", array( "team" ), $args );
 
-  /********************************
-  	* EVENT CATEGORIES
-  ********************************/
+	/**
+	 * Taxonomy: Event Categories.
+	 */
 
 	$labels = array(
 		"name" => __( "Event Categories", "uncode-child" ),
@@ -315,9 +182,9 @@ function cptui_register_my_taxes() {
 	);
 	register_taxonomy( "event_category", array( "events" ), $args );
 
-  /********************************
-  	* PRESS WRITEUP CATEGORIES
-  ********************************/
+	/**
+	 * Taxonomy: Press Writeup Categories.
+	 */
 
 	$labels = array(
 		"name" => __( "Press Writeup Categories", "uncode-child" ),
@@ -342,9 +209,9 @@ function cptui_register_my_taxes() {
 	);
 	register_taxonomy( "press_category", array( "press" ), $args );
 
-  /********************************
-  	* CAREER CATEGORIES
-  ********************************/
+	/**
+	 * Taxonomy: Career Categories.
+	 */
 
 	$labels = array(
 		"name" => __( "Career Categories", "uncode-child" ),
@@ -369,9 +236,9 @@ function cptui_register_my_taxes() {
 	);
 	register_taxonomy( "career_categories", array( "careers" ), $args );
 
-  /********************************
-  	* LOCATION CATEGORIES
-  ********************************/
+	/**
+	 * Taxonomy: Location Categories.
+	 */
 
 	$labels = array(
 		"name" => __( "Location Categories", "uncode-child" ),
@@ -395,6 +262,33 @@ function cptui_register_my_taxes() {
 		"show_in_quick_edit" => false,
 	);
 	register_taxonomy( "location_category", array( "locations" ), $args );
+
+	/**
+	 * Taxonomy: Technology Solution Categories.
+	 */
+
+	$labels = array(
+		"name" => __( "Technology Solution Categories", "uncode-child" ),
+		"singular_name" => __( "Technology Solution Category", "uncode-child" ),
+	);
+
+	$args = array(
+		"label" => __( "Technology Solution Categories", "uncode-child" ),
+		"labels" => $labels,
+		"public" => true,
+		"hierarchical" => false,
+		"label" => "Technology Solution Categories",
+		"show_ui" => true,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"query_var" => true,
+		"rewrite" => array( 'slug' => 'technology_solution_categories', 'with_front' => true, ),
+		"show_admin_column" => false,
+		"show_in_rest" => false,
+		"rest_base" => "",
+		"show_in_quick_edit" => false,
+	);
+	register_taxonomy( "technology_solution_categories", array( "solutions" ), $args );
 }
 
 add_action( 'init', 'cptui_register_my_taxes' );
